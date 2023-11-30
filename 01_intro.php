75a250bc8aecd4a7ce1adcc290da43311bc079fa
ashishp101 <ashishpanwar413@gmail.com>
2023-11-30 19:27:58 +0530

    Introduction

---
+++ 01_intro.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Learn More Everyday <br> ";
+    print("Learn Earn");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Basics</title>
+</head>
+<body>
+	<br>
+	<button>Order</button>
+</body>
+</html>
